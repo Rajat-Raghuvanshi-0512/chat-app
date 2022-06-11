9f2245034215d784ab198147d9cafb776d176386
Rajat Raghuvanshi <rajatraghuvanshi1507@gmail.com>
2022-06-11 15:26:47 +0530

    initialized project

---
+++ src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase/app"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCsG4NcOZc8LcaBGYb3I2lUlGfv7fB7MzU",
+    authDomain: "chat-app-c8964.firebaseapp.com",
+    projectId: "chat-app-c8964",
+    storageBucket: "chat-app-c8964.appspot.com",
+    messagingSenderId: "663403965115",
+    appId: "1:663403965115:web:300d8de8d830a44312d79e",
+    measurementId: "G-YBKB2G5XTY"
+};
+
+const app = firebase.initializeApp(firebaseConfig);
